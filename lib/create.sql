33a20030a2d90915da9aef26bae00fd240dd0faf
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-28 01:51:31 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE projects (
+
+  id            INTEGER PRIMARY KEY,
+  title         TEXT,
+  category      TEXT,
+  funding_goal  INTEGER,
+  start_date    INTEGER,
+  end_date      INTEGER
+  
+                        );
+
+CREATE TABLE users (
+
+  id            INTEGER PRIMARY KEY,
+  name          TEXT,
+  category      TEXT,
+  funding_goal  INTEGER,
+  start_date    INTEGER,
+  end_date      INTEGER
+  
+                        );
+                        
+                        
