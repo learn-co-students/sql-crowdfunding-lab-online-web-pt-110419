b2d350deea46a07bc63e86a370ae69e93d7f5bdd
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-28 15:07:35 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -4,8 +4,8 @@ CREATE TABLE projects (
   title         TEXT,
   category      TEXT,
   funding_goal  INTEGER,
-  start_date    INTEGER,
-  end_date      INTEGER
+  start_date    TEXT,
+  end_date      TEXT
   
                         );
 
---
+++ lib/insert.sql
@@ -0,0 +1,40 @@
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+                    
+                    VALUES("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020"),
+                          ("ruby", "a", "a", "10/10/2020", "10/11/2020");
+                          
+
+INSERT INTO users(name, age)
+
+                  VALUES("John",     27),
+                        ("Jack",     26),
+                        ("Jason",    27),
+                        ("Bill",     28),
+                        ("Jackie",   28),
+                        ("Terrance", 29),
+                        ("Lawrence", 30),
+                        ("Pierre",   25),
+                        ("Sara",     30),
+                        ("Valerie",  31),
+                        ("John",     27),
+                        ("Jack",     26),
+                        ("Jason",    27),
+                        ("Bill",     28),
+                        ("Jackie",   28),
+                        ("Terrance", 29),
+                        ("Lawrence", 30),
+                        ("Pierre",   25),
+                        ("Sara",     30),
+                        ("Valerie",  31);
+                        
+                        
+                        
+          
